496663e1b8631a2ad74f4cd0f088ea7e2257c4be
maciektbt4 <mbkrzak@gmail.com>
2022-08-28 10:32:29 +0200

    Revert "commit"
    This reverts commit 74c85398e0a957d6e66dc83ef29fff35e08f8ef5.

--- main.py
+++ main.py
@@ -1,38 +1,14 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, ReferenceListProperty
-from kivy.vector import Vector
-from kivy.clock import Clock
 
-class PongBall(Widget):
-
-    # velocity of the ball on x and y axis
-    velocity_x = NumericProperty(0)
-    velocity_y = NumericProperty(0)
-
-    # referencelist property so we can use ball.velocity as
-    # a shorthand, just like e.g. w.pos for w.x and w.y
-    velocity = ReferenceListProperty(velocity_x, velocity_y)
-
-    # ``move`` function will move the ball one step. This
-    #  will be called in equal intervals to animate the ball
-    def move(self):
-        self.pos = Vector(*self.velocity) + self.pos
 
 class PongGame(Widget):
-    
-    def update(self, dt):
-        # call ball.move and other stuff
-        pass    
-    
+    pass
 
 
 class PongApp(App):
     def build(self):
-        game = PongGame()
-        Clock.schedule_interval(game.update, 1.0/60.0)
-        return game
-
+        return PongGame()
 
 
 if __name__ == '__main__':
